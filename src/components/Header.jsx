f9e348ec6aeaf590faa40e3dc10274552e890c31
Gheorghe <madangheorghe.30@gmail.com>
2025-02-24 15:38:41 +0100

    consegna es + bonus

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,10 @@ import NavBar from "./headerComponents/NavBar"
 // Header.jsx
 export default function Header() {
     return (
-        <>
-            <h1>ciao</h1>
+        <header>
             <NavBar />
-        </>
+        </header>
+
+
     )
 }
--- src/components/headerComponents/NavBar.jsx
+++ src/components/headerComponents/NavBar.jsx
@@ -3,10 +3,10 @@ import { NavLink, Link } from "react-router-dom"
 export default function NavBar() {
     return (
         <div>
-            <NavLink to='/'>HOME</NavLink>
-            <NavLink to='posts'>POSTS</NavLink>
-            <NavLink to='chiSiamo'>CHI SIAMO</NavLink>
-            <NavLink to='scopriDiPiu'>SCOPRI DI PIU'</NavLink>
+            <li><NavLink to='/'>HOME</NavLink></li>
+            <li><NavLink to='posts'>POSTS</NavLink></li>
+            <li><NavLink to='chiSiamo'>CHI SIAMO</NavLink></li>
+            <li><NavLink to='scopriDiPiu'>SCOPRI DI PIU'</NavLink></li>
         </div>
     )
 }
